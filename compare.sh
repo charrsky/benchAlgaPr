e10cba41764a6e59d20da4d65d19e9141eaf125d
Alexandre Moine <alexandre@moine.me>
2018-07-08 22:40:10 +0200

    Can test nonEmpty

--- compare.sh
+++ compare.sh
@@ -2,8 +2,9 @@
 
 #ARG 1: Stack or Cabal
 #ARG 2: local repo (will be copied in /tmp)
-#ARG 3: Output: Html or Ascii
-#ARG 4: COMMIT ID to rest alga
+#ARG 3: Output: Html, Ascii or QuickComparison
+#ARG 4: COMMIT ID to reset alga
+#ARG 5: (Optional, needed if trying to bench specific functions (see below))If set to "True", will bench Alga.Graph.NonEmpty instead of Alga.Graph
 #Other Args: Functions to benchmark
 
 BENCHPR="BENCHPR"
@@ -11,9 +12,18 @@ NAME="algebraic-graphs"
 BGVERSION="bench-graph-0.1.0.0"
 
 if [ "$#" -lt 4 ]; then
-    echo "You must enter more than 4 command line arguments"
+    echo "You must enter more than 3 command line arguments"
     exit 1
 fi
+
+if [ "$5" = "True" ]
+then
+  FILE="Algebra/Graph/NonEmpty.hs"
+else
+  FILE="Algebra/Graph.hs"
+fi
+
+FILEHS=$(echo "$FILE" | sed 's/.\{3\}$//' | sed 's|/|.|g')
 
 rm -rf /tmp/$BENCHPR
 mkdir /tmp/$BENCHPR
@@ -36,10 +46,16 @@ git reset --hard $4
 echo "AlgaOld Commit ID: $(git rev-parse HEAD)"
 
 sed -i "s/$NAME/old/g" $NAME.cabal
-sed -i "s/Algebra.Graph,/Algebra.GraphOld,/g" $NAME.cabal
 mv $NAME.cabal old.cabal
-mv src/Algebra/Graph.hs src/Algebra/GraphOld.hs
-find . -type f -iname '*.hs' -exec sed -i "s/Algebra.Graph[^.]/Algebra.GraphOld/g" "{}" +;
+
+for n in "Algebra/Graph.hs" "Algebra/Graph/NonEmpty.hs"
+do
+  nPrime=$(echo "$n" | sed 's/.\{3\}$//' | sed 's|/|.|g')
+  sed -i "s/$nPrime,/${nPrime}Old,/g" old.cabal
+  mv "src/$n" "src/$(echo "$n" | sed 's/.\{3\}$//')Old.hs"
+  find . -type f -iname '*.hs' -exec sed -i "s/$nPrime[^.]/${nPrime}Old/g" "{}" +;
+done
+
 cd ..
 
 if [ $1 = "Stack" ]
@@ -52,13 +68,28 @@ else
 echo "packages: \".\" old/ alga/" > cabal.project
 fi
 
-sed -i "s/Alga.Graph/Alga.Graph,Alga.GraphOld/g" bench-graph.cabal
+sed -i "s/$FILEHS/$FILEHS,${FILEHS}Old/g" bench-graph.cabal
 sed -ri "s/$NAME(.*)/old, $NAME\1/g" bench-graph.cabal
 
 cp bench/Alga/Graph.hs bench/Alga/GraphOld.hs
 
 sed -i "s/Alga.Graph/Alga.GraphOld/g" bench/Alga/GraphOld.hs
-sed -i "s/Algebra.Graph/Algebra.GraphOld/g" bench/Alga/GraphOld.hs
+sed -i "s/Algebra.Graph/${FILEHS}Old/g" bench/Alga/GraphOld.hs
+sed -i "s/Algebra.Graph/${FILEHS}/g" bench/Alga/Graph.hs
+
+if [ "$5" = "True" ]
+then
+  for n in "bench/Alga/Graph.hs" "bench/Alga/GraphOld.hs"
+  do
+    sed -i "s/import BenchGraph.Types/import BenchGraph.Types\nimport qualified Data.List.NonEmpty as L/" $n
+    sed -i 's/Graph Int/NonEmptyGraph Int/g' $n
+    sed -i 's/mk = edges/mk = edges1 . L.fromList/' $n
+    sed -i 's/clique/clique1 $ L.fromList/g' $n
+    sed -i 's/, S.isEmpty isEmpty//g' $n
+    sed -i 's/ vertexList/ vertexList1/g' $n
+    sed -i  's/ removeVertex/ removeVertex1/g' $n
+  done
+fi
 
 sed -i "s/import qualified Alga.Graph/import qualified Alga.Graph\nimport qualified Alga.GraphOld/g" bench/ListS.hs
 sed -i "s/(\(\"Alga\", map Shadow Alga.Graph.functions \))/\(\1\),\(\"AlgaOld\", map Shadow Alga.GraphOld.functions \)/g" bench/ListS.hs
@@ -75,7 +106,7 @@ fi
 
 STR=""
 
-ARGS=$(echo "$@" | cut -d' ' -f5-)
+ARGS=$(echo "$@" | cut -d' ' -f6-)
 
 for var in $ARGS
 do
